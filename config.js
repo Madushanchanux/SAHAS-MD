914af8656db5ad4461116954d0491ccabd1772a3
Chanuka-madushan <chanukamadushan889@gmail.com>
2024-11-07 23:23:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "Put Session Id",
+  SESSION_ID: process.env.SESSION_ID || "j6J1RIrA#AL_h3IyvUnetQAg08AJSkyhXJ84iolPfZZMYBJoxNzU",
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
     // ADD Your MongoDB Database URL
